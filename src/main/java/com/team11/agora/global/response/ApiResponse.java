31f3418e8bbdde9e62b5cbb46a252d9e6e530549
116_Lv <lbw0109@gmail.com>
2026-06-23 11:46:10 +0900

    feat: 공통응답형식 생성

---
+++ src/main/java/com/team11/agora/global/response/ApiResponse.java
@@ -0,0 +1,16 @@
+package com.team11.agora.global.response;
+
+public record ApiResponse<T>(
+        String status,
+        String message,
+        T data
+) {
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>("SUCCESS", message, data);
+    }
+
+    public static ApiResponse<Void> error(String message) {
+        return new ApiResponse<>("ERROR", message, null);
+    }
+}
